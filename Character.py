d231302848d85f29d0cc262d5602291574b58c9e
alisonhc <achi@macalester.edu>
2017-12-10 16:45:38 -0600

    made train and test set, fixed some compatibility issues

--- Character.py
+++ Character.py
@@ -7,7 +7,7 @@ class Character:
         self.cornell_data = DataSource.CORNELL_MOVIE_CORPUS.value
 
     def most_common(self, num=5):
-        with open(self.cornell_data.localPath / "movie_lines.txt", errors="ignore") as f:
+        with open(str(self.cornell_data.localPath / "movie_lines.txt"), errors="ignore") as f:
             char_ids = [line.split("+++$+++")[1].strip() for line in f]
         most_common = []
         most_common.extend(Counter(char_ids).most_common(num))
--- data_utils.py
+++ data_utils.py
@@ -48,7 +48,7 @@ def basic_tokenizer(sentence):
   """Very basic tokenizer: split the sentence into a list of tokens."""
   words = []
   for space_separated_fragment in sentence.strip().split():
-    words.extend(re.split(_WORD_SPLIT, space_separated_fragment))
+    words.extend(re.split(_WORD_SPLIT, space_separated_fragment.encode()))
   return [w for w in words if w]
 
 
